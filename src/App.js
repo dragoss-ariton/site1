572c63dee66a3e33bcb230d3ab7bd8a3d8ef7e30
dragoss-ariton <106492837+dragoss-ariton@users.noreply.github.com>
2024-11-02 16:07:31 +0000

    Adição da pag HTML para o component JS, alteração das rotas(links), adição do react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import EscolaDeConducao from './components/EscolaConducao'; // Certifique-se de que este caminho e nome do arquivo estão corretos
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/escola-de-conducao" element={<EscolaDeConducao />} /> {/* Rota para a nova página */}
+            </Routes>
+        </Router>
+    );
+};
 
 export default App;
