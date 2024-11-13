ca7d758e187103ef170f980d5bb5fcb817734c70
Agosto <tiago0reis0eliseu@gamail.com>
2024-11-13 13:11:15 +0000

    nigga

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
 // src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Importações das páginas
-import Navbar from './components/paginas/Navbar'; // Fixed import name
-import Hero from './components/paginas/hero';
-import InicioPnd from './components/paginas/Inicio_PND';
-import ServicosPnd from './components/paginas/Servicos_PND';
-import EnsinoPnd from './components/paginas/Ensino_PND';
-import FaqPnd from './components/paginas/Faq_PND';
-import ContactosPnd from './components/paginas/Contactos_PND';
+import Navbar from "./components/paginas/Navbar"; // Fixed import name
+import Hero from "./components/paginas/hero";
+import InicioPnd from "./components/paginas/Inicio_PND";
+import ServicosPnd from "./components/paginas/Servicos_PND";
+import EnsinoPnd from "./components/paginas/Ensino_PND";
+import FaqPnd from "./components/paginas/Faq_PND";
+import ContactosPnd from "./components/paginas/Contactos_PND";
 
-import './App.css';
+import "./App.css";
 
 const App = () => {
-    return (
-        <Router>
-            <div className="main-content">
-                <Routes>
-                    <Route path="/" element={<Hero />} />
-                    <Route path="/Contactos_PND" element={<ContactosPnd />} />
-                    <Route path="/Inicio_PND" element={<InicioPnd />} />
-                    <Route path="/Servicos_PND" element={<ServicosPnd />} />
-                    <Route path="/Ensino_PND" element={<EnsinoPnd />} />
-                    <Route path="/FAQ-parque-das-nacoes" element={<FaqPnd />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      {/* Navbar will be displayed on all pages */}
+      <Navbar />
+
+      <div className="main-content">
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/Contactos_PND" element={<ContactosPnd />} />
+          <Route path="/Inicio_PND" element={<InicioPnd />} />
+          <Route path="/Servicos_PND" element={<ServicosPnd />} />
+          <Route path="/Ensino_PND" element={<EnsinoPnd />} />
+          <Route path="/FAQ-parque-das-nacoes" element={<FaqPnd />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
