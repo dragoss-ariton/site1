08aa927a5dd23be0f74cf6d6d6b730c9c5fb8d70
dragoss-ariton <106492837+dragoss-ariton@users.noreply.github.com>
2024-12-08 19:45:19 +0000

    Navbar responsiva

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// src/App.js
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 
 // Importações das páginas
 import Navbar from "./components/paginas/Navbar";
@@ -24,29 +25,46 @@ import ContactosPdn from "./components/paginas/Contactos_PDN";
 import ScrollToTop from "./components/paginas/ScrollToTop"; 
 
 import "./App.css";
+import "./components/css/Navbar.css";
+
+const AppContent = () => {
+  const location = useLocation(); // Pega a localização da rota atual
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+        // Remover o padding-top na página inicial
+        document.body.style.paddingTop = '0';
+    } else {
+        // Definir o padding-top quando não for a página inicial
+        document.body.style.paddingTop = '55px'; // Ou qualquer outro valor
+    }
+  }, [location]);
+
+  return (
+    <div className="main-content">
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/Inicio-PDN" element={<InicioPdn />} />
+        <Route path="/Inicio-OL" element={<InicioOl />} />
+        <Route path="/Ensino-PDN" element={<EnsinoPdn />} />
+        <Route path="/Ensino-OL" element={<EnsinoOL />} />
+        <Route path="/Faq-PDN" element={<FaqPDN />} />
+        <Route path="/Faq-OL" element={<FaqOL />} />
+        <Route path="/Contactos-PDN" element={<ContactosPdn />} />
+        <Route path="/Contactos-OL" element={<ContactosOL />} />
+        <Route path="/Equipa-OL" element={<EquipaOL />} />
+        <Route path="/Equipa-PDN" element={<EquipaPDN />} />
+      </Routes>
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <Router>
       <ScrollToTop />
-      <div>
       <Navbar />
-      </div>
-      <div className="main-content">
-        <Routes>
-          <Route path="/" element={<Hero />} />
-          <Route path="/Inicio-PDN" element={<InicioPdn />} />
-          <Route path="/Inicio-OL" element={<InicioOl />} />
-          <Route path="/Ensino-PDN" element={<EnsinoPdn />} />
-          <Route path="/Ensino-OL" element={<EnsinoOL />} />
-          <Route path="/Faq-PDN" element={<FaqPDN />} />
-          <Route path="/Faq-OL" element={<FaqOL />} />
-          <Route path="/Contactos-PDN" element={<ContactosPdn />} />
-          <Route path="/Contactos-OL" element={<ContactosOL />} />
-          <Route path="/Equipa-OL" element={<EquipaOL />} />
-          <Route path="/Equipa-PDN" element={<EquipaPDN />} />
-        </Routes>
-      </div>
+      <AppContent /> {/* Separado para garantir o uso correto do Router */}
       <Footer />
     </Router>
   );
