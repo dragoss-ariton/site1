7097eb62fe6eda771445c6a8a948975fb1abb6dd
dragoss-ariton <106492837+dragoss-ariton@users.noreply.github.com>
2024-12-16 20:21:21 +0000

    Serviços done

--- src/components/paginas/Ensino_PDN.jsx
+++ src/components/paginas/Ensino_PDN.jsx
@@ -5,32 +5,39 @@ import Infra3PDN from '../assets/Infra3PDN.jpg';
 const PopupBoxes = () => {
   const [activePopup, setActivePopup] = useState(null);
 
-  // Box data with titles, images, and descriptions
   const boxes = [
     {
-        id: 0,
-        title: "Categorias lecionadas",
-        imgSrc: Infra3PDN,
-        description: ["Temos carta para Ligeiros Motociclos Cat B, A, A2 e A1"],
-      },
-      {
-        id: 1,
-        title: "Pacote",
-        imgSrc: Infra3PDN,
-        description: [{ title: "O que inclui?", content: ["Inscrição","Aulas de código ilimitadas","32 aulas de código em situação real de trânsito(sem simulador)","Exame Teórico e Exame Prático pelo privado(vantagem de poder escolher o dia e hora que quer fazer exame)","Oferta do livro de código"] }],
-      },
-      {
-        id: 2,
-        title: "Treino a encartados",
-        imgSrc: Infra3PDN,
-        description: ["Volte a conduzir com confiança com as nossas aulas de treino."],
-      },
-      {
-        id: 3,
-        title: "English driving course",
-        imgSrc: Infra3PDN,
-        description: ["On Category B cards in English, the codebook is included, this translated from Portuguese to English.", "For category A, A1 and A2 (motorcycle) in English, we only have the book in Portuguese.","For both categories in english, the medical certificate is included."],
-      },
+      id: 0,
+      title: "Categorias lecionadas",
+      imgSrc: Infra3PDN,
+      description: ["Temos carta para Ligeiros Motociclos Cat B, A, A2 e A1"],
+    },
+    {
+      id: 1,
+      title: "Pacote",
+      imgSrc: Infra3PDN,
+      description: [{ title: "O que inclui?", content: ["Inscrição","Aulas de código ilimitadas","32 aulas de código em situação real de trânsito(sem simulador)","Exame Teórico e Exame Prático pelo privado(vantagem de poder escolher o dia e hora que quer fazer exame)","Oferta do livro de código"] }],
+    },
+    {
+      id: 2,
+      title: "Horários",
+      imgSrc: Infra3PDN,
+      description: [
+        { title: "Código:", content: ["2ª a 6ª às 12h, 17h, 18h, 19h", "Sábados às 10h, 11h, 12h"] },
+        { title: "Condução:", content: ["2ª a 6ª das 7h às 20h", "Sábados das 7h às 13h"] },
+        { title: "Secretária:", content: ["2ª a 6ª das 10h às 13h / 14.30h às 20h", "Sábados das 10h às 13h"] },
+      ],
+    },
+    {
+      id: 3,
+      title: "English driving course",
+      imgSrc: Infra3PDN,
+      description: [
+        "On Category B cards in English, the codebook is included, this translated from Portuguese to English.",
+        "For category A, A1 and A2 (motorcycle) in English, we only have the book in Portuguese.",
+        "For both categories in english, the medical certificate is included.",
+      ],
+    },
   ];
 
   const handleBoxClick = (boxIndex) => {
@@ -46,28 +53,39 @@ const PopupBoxes = () => {
     <div className="services-container1">
       <h1 className='title-services1'>Os <span>Nossos</span> Serviços</h1>
       <div className="container1">
-        {/* Boxes */}
         {boxes.map((box) => (
           <div key={box.id} className="box1" onClick={() => handleBoxClick(box.id)}>
-            <div className="box-image-container1">
-              <img src={box.imgSrc} alt={box.title} className="box-image1" />
-              <div className="box-title-overlay1">
-                <p className="box-title1">{box.title}</p>
-              </div>
+          <div className="box-image-container1">
+            <img src={box.imgSrc} alt={box.title} className="box-image1" />
+            <div className="box-title-overlay1">
+              <p className="box-title1">{box.title}</p>
             </div>
           </div>
-        ))}
-      </div>
+        </div>
+      ))}
+    </div>
 
-      {/* Popup */}
       {activePopup !== null && (
         <div>
           <div className="popup1">
             <h2>{boxes[activePopup].title}</h2>
             <ul>
-              {boxes[activePopup].description.map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
+              {boxes[activePopup].description.map((item, index) => {
+                if (typeof item === "object" && item.title) {
+                  return (
+                    <div key={index}>
+                      <h3>{item.title}</h3>
+                      <ul>
+                        {item.content.map((contentItem, contentIndex) => (
+                          <li key={contentIndex}>{contentItem}</li>
+                        ))}
+                      </ul>
+                    </div>
+                  );
+                } else {
+                  return <li key={index}>{item}</li>;
+                }
+              })}
             </ul>
             <button onClick={handleClosePopup}>Close</button>
           </div>
