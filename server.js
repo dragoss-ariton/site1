182460ab4fb294e21245b2ac51e38badd36f723f
dragoss-ariton <106492837+dragoss-ariton@users.noreply.github.com>
2024-12-17 21:42:26 +0000

    server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-// server.js
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const emailRoutes = require('./emailRoutes'); // Rotas do NodeMailer
 
 const app = express();
@@ -12,6 +12,14 @@ app.use(express.json());
 
 // Configurar a rota de envio de e-mails
 app.use('/api', emailRoutes);
+
+// Servir arquivos estáticos do React
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Redirecionar todas as outras requisições para o React
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 // Iniciar o servidor
 app.listen(PORT, () => {
