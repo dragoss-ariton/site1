ef70ef46f55945c35b13e22c6dd46f82fa223330
unknown <miguelbferreira5@gmail.com>
2024-12-29 01:56:22 +0000

    Test Mailer

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const emailRoutes = require('./emailRoutes'); // Rotas do NodeMailer
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors());
